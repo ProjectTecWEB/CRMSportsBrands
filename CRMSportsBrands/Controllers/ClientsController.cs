714ff4ed3fd01883964064804bd221ed765b8f96
santytorrico <90476969+santytorrico@users.noreply.github.com>
2022-06-02 10:35:47 -0400

    Added Clients

---
+++ CRMSportsBrands/Controllers/ClientsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRMSportsBrands.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        public IActionResult GetClients()
+        {
+            return null;
+        }
+    }
+}
