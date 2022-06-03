2eac2a0797023405767f894893466b42061fed98
adrianBalderramaf <adrian.balderrama@ucb.edu.bo>
2022-06-03 17:26:20 -0400

    Finaal

---
+++ BackingServices/Models/ClientesMo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackingServices.Models
+{
+    public class ClientesMo
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string Adress { get; set; }
+    }
+}
