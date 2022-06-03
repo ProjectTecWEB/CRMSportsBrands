248c614d53c08e637d5a5ad00c8ffa2aa3b91c2a
adrianBalderramaf <adrian.balderrama@ucb.edu.bo>
2022-06-02 20:15:07 -0400

    Implementacion modelos

---
+++ BackingServices/Models/ClientesMo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BackingServices.Models
+{
+    public class ClientesMo
+    {
+        public int id { get; set; }
+        public string uid { get; set; }
+        public string valid_us_ssn { get; set; }
+        public string invalid_us_ssn { get; set; }
+    }
+}
---
+++ PracticeThree/Controllers/ClientesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Logic.Managers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CsmSport.Controllers
+{
+    [Route("Clientes-management")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private ClientesManager _userManager;
+        public ClientesController(ClientesManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [Route("Clientes")]
+        public IActionResult GetClientes() 
+        {
+            return Ok(_userManager.GetClientes());
+        }
+
+        [HttpGet]
+        [Route("id-clientes")]
+        public IActionResult GetIdNumber()
+        {
+            return Ok(_userManager.GetSSN());
+        }
+    }
+}
